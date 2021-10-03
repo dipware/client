3da41f08bfb5c7c9b34d9913c79826053395ab81
Matthew Dippel <mdip226@gmail.com>
2021-10-03 18:27:14 -0400

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -39,16 +32,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Client httpClient;
+  late Web3Client ethClient;
+  bool data = false;
+
+  final myAddress = '0x79336570b5E3fae0Db6198EDbD8128895B798182';
+
+  @override
+  void initState() {
+    super.initState();
+    httpClient = Client();
+    ethClient = Web3Client(
+        "https://rinkeby.infura.io/v3/4219bd2c584d4e788aefb96b72a3f5c9",
+        httpClient);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text('You have pushed the button this many times:'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          data
+              ? const Text('results')
+              : const Center(child: CircularProgressIndicator()),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(onPressed: () {}, child: const Text("Yes")),
+              TextButton(onPressed: () {}, child: const Text("No")),
+              TextButton(onPressed: () {}, child: const Text("Refresh")),
+            ],
+          ),
+        ],
       ),
     );
   }
